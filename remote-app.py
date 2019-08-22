bf50f6d3cc46347d134874a402ecbf8242b2615a
kishra <kishore.lankapali@gmail.com>
2019-08-22 00:14:14 -0700

    Update output to a list data type and code update

--- remote-app.py
+++ remote-app.py
@@ -3,11 +3,11 @@ The application is responsible for identifying the keys entered while searching 
 
 You have your a keyboard as below
 [
-    ['a', 'b', 'c', 'd', 'e'], 
-    ['f', 'g', 'h', 'i', 'j'], 
-    ['k', 'l', 'm', 'n', 'o'], 
-    ['p', 'q', 'r', 's', 't'], 
-    ['u', 'v', 'w', 'x', 'y'], 
+    ['a', 'b', 'c', 'd', 'e'],
+    ['f', 'g', 'h', 'i', 'j'],
+    ['k', 'l', 'm', 'n', 'o'],
+    ['p', 'q', 'r', 's', 't'],
+    ['u', 'v', 'w', 'x', 'y'],
     ['z']
 ]
 
@@ -23,57 +23,43 @@ Use the below script to find out.
 import string
 keys = string.ascii_lowercase	#### Create as string of lowercase letter
 keys_list = [i for i in keys]	#### Convert the string to list
+output = []
 
-def finder(start_value, input_list):
-    if input_list:
-        i = input_list[0]
-        index = keys_list.index(i) 
+def keys_sp_finder(keys_list, input_string, keys_dict={}):
+    initial_position = [0,0]
+    for i in keys:
+        index = keys_list.index(i)
         row = index / 5 #Find the row of the string
         column = index % 5 #Find the column of the string
-        dst_value = [row, column]
+        let_position = [row, column]
+        keys_dict[i] = {'position': let_position}
 
-	#### Our start_value is at "a". Hence we started with [0,0] (Row=0 and Column=0).
-        while start_value != dst_value:
-            if dst_value[0] - start_value[0] > 0:
-                print("down " * (dst_value[0]-start_value[0]))
-                start_value[0] = dst_value[0]
-                if dst_value[1] - start_value[1] > 0:
-                    print("right "*(dst_value[1] - start_value[1]))
-                    print("Enter")
-                    start_value[1]=dst_value[1]
-                elif dst_value[1] - start_value[1] < 0:
-                    print("left "*((dst_value[1] - start_value[1])*-1))
-                    print("Enter ")
-                    start_value[1]=dst_value[1]
-                else:
-                    print("Enter ")
-                    start_value[1] = dst_value[1]
-            elif dst_value[0] - start_value[0] < 0:
-                print("up "*((start_value[0]-dst_value[0])*1))
-                start_value[0] = dst_value[0]
-                if dst_value[1] - start_value[1] > 0:
-                    print("right "*(dst_value[1] - start_value[1]))
-                    print("Enter ")
-                    start_value[1] = dst_value[1]
-                elif dst_value[1] - start_value[1] < 0:
-                    print("left "*((dst_value[1] - start_value[1])*-1))
-                    print("Enter ")
-                    start_value[1] = dst_value[1]
-                else:
-                    print("Enter ")
-                    start_value[1]=dst_value[1]
-            else:
-                if dst_value[1] - start_value[1] > 0:
-                    print("right "*(dst_value[1] - start_value[1]))
-                    print("Enter ")
-                    start_value[1] = dst_value[1]
-                elif dst_value[1] - start_value[1] < 0:
-                    print("left "*((dst_value[1] - start_value[1])*-1))
-                    print("Enter")
-                    start_value[1] = dst_value[1]
-                else:
-                    print("Enter ")
-                    start_value[1] = dst_value[1]
-        finder(start_value, input_list[1:])
+    while len(input_string) > 0:
+        let = input_string[0]
+        let_position = keys_dict[let]["position"]
+        cursor = [let_position[0] - initial_position[0], let_position[1] - initial_position[1]]
+        down = ["down"]*cursor[0]
+        right = ["right"]*cursor[1]
+        up = ["up"]*-cursor[0]
+        left = ["left"]*-cursor[1]
+        enter = ["enter"]
+        while initial_position[0] != let_position[0]:
+            if cursor[0] > 0:
+                output.extend(down)
+                initial_position[0] = let_position[0]
+            elif cursor[0] < 0:
+                output.extend(up)
+                initial_position[0] = let_position[0]
+            while initial_position[1] != let_position[1]:
+                if cursor[1] > 0:
+                    output.extend(right)
+                    initial_position[1] = let_position[1]
+                elif cursor[1] < 0:
+                    output.extend(left)
+                    initial_position[1] = let_position[1]
+        else:
+            output.extend(enter)
+        input_string = input_string[1:]
 
-finder([0,0], "netflix")
+keys_sp_finder(keys_list, "whatis")
+print(output)
